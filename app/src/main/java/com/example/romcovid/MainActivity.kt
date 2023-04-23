05038656edece21276da8a5acc9987b62f28c216
Sebastian Utoiu <sebiutoiu12@gmail.com>
2023-04-23 20:32:22 +0300

    Initial commit

---
+++ app/src/main/java/com/example/romcovid/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.romcovid
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.romcovid.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupNav()
+    }
+
+    private fun setupNav() {
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        findViewById<BottomNavigationView>(R.id.nav_view)
+            .setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.navigation_home,
+                R.id.navigation_dashboard,
+                R.id.navigation_information,
+                R.id.navigation_settings -> showBottomNav()
+                    else -> hideBottomNav()
+            }
+        }
+    }
+
+    private fun showBottomNav() {
+        binding.navView.visibility = View.VISIBLE
+    }
+
+    private fun hideBottomNav() {
+        binding.navView.visibility = View.GONE
+    }
+
+}
---
+++ app/src/main/java/com/example/romcovid/ui/settings/SettingsViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.romcovid.ui.settings
+
+import androidx.lifecycle.ViewModel
+
+class SettingsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
